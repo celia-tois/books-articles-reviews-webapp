c46d50be6c7c341fa4e9fa85eb80e1e4b5aefeea
Célia Tois <celia.tois@gmail.com>
2022-05-09 15:53:32 +0200

    Install pillow and create the app models

--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -18,10 +18,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 import authentication.views
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(template_name='authentication/login.html', redirect_authenticated_user=True), name='login'),
     path('signup/', authentication.views.signup, name='signup'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('home/', app.views.home, name='home'),
 ]
