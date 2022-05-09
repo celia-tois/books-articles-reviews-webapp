b4b1db5cc38a5faa03937567efb9a146a85d25a6
Célia Tois <celia.tois@gmail.com>
2022-05-09 14:49:05 +0200

    Create the login and signup pages

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from . import forms
 
-# Create your views here.
+
+def signup(request):
+    form = forms.SignUpForm()
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+import authentication.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='authentication/login.html', redirect_authenticated_user=True), name='login'),
+    path('signup/', authentication.views.signup, name='signup'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
