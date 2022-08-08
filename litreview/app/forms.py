86d4660f2c063d6ec72c1c2eea241a1fb8069324
Célia Tois <celia.tois@gmail.com>
2022-08-08 16:44:36 +0200

    Remove the image property

--- litreview/app/forms.py
+++ litreview/app/forms.py
@@ -3,8 +3,6 @@ from app.models import Ticket, Review
 
 
 class TicketForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.FileInput)
-
     class Meta:
         model = Ticket
         exclude = ('user',)
