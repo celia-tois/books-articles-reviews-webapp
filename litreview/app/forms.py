9d273c478d7529a377a48cbe47f318a359565678
Célia Tois <celia.tois@gmail.com>
2022-08-08 11:02:54 +0200

    Move the image property to the right class

--- litreview/app/forms.py
+++ litreview/app/forms.py
@@ -3,14 +3,14 @@ from app.models import Ticket, Review
 
 
 class TicketForm(forms.ModelForm):
+    image = forms.ImageField(widget=forms.FileInput)
+
     class Meta:
         model = Ticket
         exclude = ('user',)
 
 
 class ReviewForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.FileInput)
-
     class Meta:
         model = Review
         exclude = ('user', 'ticket')
